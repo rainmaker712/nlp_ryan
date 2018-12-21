a353907cef0b5fee97b00caf129dc0fd29b98cd5
Ryan S. Shin <sungjin7127@gmail.com>
2018-12-21 21:30:41 +0900

    scala setting

--- Scala/scala-spark/src/main/scala/SimpleApp.scala
+++ Scala/scala-spark/src/main/scala/SimpleApp.scala
@@ -3,7 +3,22 @@ import org.apache.spark.{SparkConf, SparkContext}
 object SimpleApp {
 
   def main(args: Array[String]): Unit = {
-    val logFile = "Readm"
+    val logFile = "README.md"
+
+    val conf = new SparkConf().setAppName("Simplae App").setMaster("local")
+
+    val sc = new SparkContext(conf)
+
+    val logData = sc.textFile(logFile, 2).cache()
+
+
+    val lineCount = logData.count()
+    val lineCollect = logData.collect()
+
+    println(lineCollect)
+    println("for test", lineCount)
+    sc.stop()
+
   }
 
 }
